aca08309fba8f3e8c65e5cea3fc6bedac6258348
Henry Huynh <henryqhuynh@gmail.com>
2023-07-10 15:39:41 -0700

    Complete the simple app with split, reverse, and join method in JS

---
+++ script.js
@@ -0,0 +1,34 @@
+// Relevant DOM elements
+const button = document.querySelector("#btn");
+const inputText = document.querySelector("#input-text");
+const result = document.querySelector("#result");
+
+// Adding Event Listener to Button
+button.addEventListener("click", () => {
+   // Input value and trim empty spaces
+   const text = inputText.value.trim();
+
+   // An alert for empty input
+   if (text.length == 0) {
+      alert("Input cannot be empty!");
+      return;
+   }
+
+   // Remove any  alphanumeric characters and convert the word into loswercase
+   const cleanText = text.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+   // Check if adjusted text is a palindrome by comparing original and reversed strings
+   const isPalindrome = cleanText === cleanText.split("").reverse().join("");
+
+   // Final message  based on if input is a palindrome or not
+   const message = isPalindrome ? 
+      `<span>Nice! </span>This is a palindrome!`
+      : `<span>Sorry! </span>This is not a palindrome.`;
+
+   // Update the results with a message
+   result.innerHTML  = message;
+   result.classList.remove("error", "success");
+   setTimeout(() => {
+      result.classList.add(isPalindrome ? "success" : "error");
+   }, 10)
+});
